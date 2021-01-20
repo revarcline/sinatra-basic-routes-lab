f901d42ddb784f4554df6bd2601cb8633496c2de
Alex Cline <rev.a.r.cline@gmail.com>
2021-01-20 00:54:58 -0600

    commit tree fa6c87f56268b671099c3e63304d64a014f783b1

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    'My name is Alex'
+  end
+
+  get '/hometown' do
+    'My hometown is Reston, Virginia'
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'The Creator Has a Master Plan' by Pharaoh Sanders"
+  end
 end
